da7ad2c77ebe46a52f2ccbefc8c6c7ec11bc1040
xXNannu09Xx <ngtty2020@gmail.com>
2026-03-12 20:35:16 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -15,4 +15,36 @@ function love.load()
 end
 
 function love.update(dt)
-    
+    if love.keyboard.isDown("right") then
+        player.x = player.x + (player.speed * dt)
+    elseif love.keyboard.isDown("left") then
+        player.x = player.x - (player.speed * dt)
+    end
+
+    player.yVelocity = player.yVelocity + (player.gravity * dt)
+    player.y = player.y + (player.yVelocity * dt)
+
+    if player.y + player.height > floorY then
+        player.y = floorY - player.height
+        player.yVelocity = 0
+        player.isGrounded = true
+    else
+        player.isGrounded = false
+    end
+end
+
+function love.keypressed(key)
+    if key == "space" or key == "up" then
+        if player.isGrounded then
+            player.yVelocity = player.jumpHeight
+        end
+    end
+end
+
+function love.draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.line(0, floorY, love.graphics.getWidth(), floorY)
+
+    love.graphics.setColor(0, 0.7, 1)
+    love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+end
